5946aeeda5854d0fb2a07700b47379791fc120a0
bjbouman <69520365+bjbouman@users.noreply.github.com>
2022-03-16 22:27:13 +0100

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -301,3 +301,31 @@ def scatter_kappas(adata, genes, show_cluster=True, cluster_name="clusters", use
                                 np.array([0, b, (b + d), d, 0]) * np.max(f), color="red" if reg == "up" else "orange")
         ax[idx, 1].set_xlim(0), ax[idx, 1].set_ylim(0)
     plt.show()
+
+########
+# MNN
+########
+
+from scipy.spatial import cKDTree
+
+def find_mutual_nn(orig, future, k=20, n_jobs=1):
+    
+    # we want the k NN of the current states (orig) in the future states (future)
+    # the returned indices need to be the indices of the future states
+    orig_NN = cKDTree(future).query(x=orig, k=k, n_jobs=n_jobs)[1]
+    # we want to check that the NN of the future states (future) are also in NN fo the current states (orig)
+    # the returned indices need to be the indices of the current states
+    future_NN = cKDTree(orig).query(x=future, k=k, n_jobs=n_jobs)[1] 
+    mnn = np.zeros(orig.shape[0]).astype(int)-1 
+    for cell in range(orig.shape[0]):
+        cell_nn = orig_NN[cell]
+
+        for i in cell_nn:
+            if cell in future_NN[i]:
+                mnn[cell]=i
+                break
+    return mnn
+
+
+    
+    
